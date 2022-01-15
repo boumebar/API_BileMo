ea2ebf51038a93820f7c3d8aff707d483680001d
boumediene <boume13@hotmail.com>
2022-01-15 01:09:42 +0100

    productController method=>show()

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializerInterface;
-use JMS\Serializer\SerializationContext;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,12 +12,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/api/products", name="api_products", methods="GET")
+     * @Route("/api/products", name="api_products_index", methods="GET")
      */
-    public function index(ProductRepository $productRepository, SerializerInterface $serializer): Response
+    public function index(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $products = $productRepository->findAll();
         $json = $serializer->serialize($products, 'json');
+        $response = new JsonResponse($json, 200, [], true);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/products/{id<\d+>}", name="api_products_show", methods="GET")
+     */
+    public function show(ProductRepository $productRepository, $id, SerializerInterface $serializer): JsonResponse
+    {
+        $product = $productRepository->find($id);
+        $json = $serializer->serialize($product, 'json');
         $response = new JsonResponse($json, 200, [], true);
 
         return $response;
